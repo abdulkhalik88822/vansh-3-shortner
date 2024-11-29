28d1b16cbc3e9308026cd99c625bd2872cb6be96
vansh524163 <157708281+vansh524163@users.noreply.github.com>
2024-11-29 12:58:04 +0530

    Update info.py

--- info.py
+++ info.py
@@ -58,7 +58,7 @@ LANGUAGES = ["hindi", "english", "telugu", "tamil", "kannada", "malayalam"]
 
 auth_channel = environ.get('AUTH_CHANNEL', '-1002076903232')
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
-SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', '@vansh_support62'))
+SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', '-1002413798207'))
 
 # bot settings
 AUTO_FILTER = is_enabled('AUTO_FILTER', True)
